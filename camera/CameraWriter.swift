49ad5a0792a1d1497e292ebf77338ad0d8cefb77
Yury <Yury@Dankes-MacBook-Air.local>
2015-08-31 03:19:48 +0200

    CameraWriter major update

--- camera/CameraWriter.swift
+++ camera/CameraWriter.swift
@@ -12,15 +12,21 @@ import AVFoundation
 class CameraWriter : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
     
     var assetWriter: AVAssetWriter? = nil
-    var videoOutput: AVCaptureOutput? = nil
+    var videoOutput: AVCaptureVideoDataOutput!
+    var videoWriter: AVAssetWriterInput!
     
     private let queue = dispatch_queue_create("CameraWriterSessionQueue", DISPATCH_QUEUE_SERIAL)
     
     override init() {
         super.init()
-        videoOutput = createCaptureOutput()
+        videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.setSampleBufferDelegate(self, queue: queue)
+        videoOutput.videoSettings = captureOutputSettings
+        videoOutput.alwaysDiscardsLateVideoFrames = true
+        
+        videoWriter = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: videoWriterSettings)
+        videoWriter.expectsMediaDataInRealTime = true
     }
-    
     
     deinit {
         stopWithCompletionHandler(nil)
@@ -31,14 +37,15 @@ class CameraWriter : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
         guard let writer = assetWriter else {
             throw NSError(domain: "CameraWriter", code: 0, userInfo: nil)
         }
-        if !writer.startWriting() {
-            throw writer.error ?? NSError(domain: "CameraWriter", code: writer.status.rawValue, userInfo: nil)
-        }
-        assetWriter?.startSessionAtSourceTime(kCMTimeZero)
+//        if !writer.startWriting() {
+//            throw writer.error ?? NSError(domain: "CameraWriter", code: writer.status.rawValue, userInfo: nil)
+//        }
+//        writer.startSessionAtSourceTime(kCMTimeZero)
     }
     
     func stopWithCompletionHandler(handler: ((url: NSURL, error: NSError?) -> Void)?) {
         if let assetWriter = assetWriter where assetWriter.status == .Writing {
+            videoWriter.markAsFinished()
             assetWriter.finishWritingWithCompletionHandler({ () -> Void in
                 if let handler = handler {
                     handler(url: self.tempFileURL, error: self.assetWriter?.error)
@@ -52,31 +59,18 @@ class CameraWriter : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
     
     // MARK: - Encoding settings
     let captureOutputSettings = [kCVPixelBufferPixelFormatTypeKey as NSString : NSNumber(int: Int32(kCVPixelFormatType_32BGRA))]
-    let pixelBufferSettings = [kCVPixelBufferPixelFormatTypeKey as String : NSNumber(int: Int32(kCVPixelFormatType_32BGRA))]
     
-    let videoWriterSettings = [AVVideoWidthKey: NSNumber(int: 640),
+    let videoWriterSettings = [
+        AVVideoWidthKey: NSNumber(int: 640),
         AVVideoHeightKey: NSNumber(int: 640),
-        AVVideoCodecKey: AVVideoCodecH264]
-    
-    // does it needed to check .connectionWithMediaType(AVMediaTypeVideo) where connection.active somewhere?
-    func createCaptureOutput() -> AVCaptureOutput {
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.setSampleBufferDelegate(self, queue: queue)
-        videoOutput.videoSettings = captureOutputSettings
-        videoOutput.alwaysDiscardsLateVideoFrames = true
-        return videoOutput
-    }
+        AVVideoCodecKey: AVVideoCodecH264,
+        AVVideoScalingModeKey : AVVideoScalingModeResizeAspectFill]
     
     var tempFileURL: NSURL = {
         let tempDirURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
         return tempDirURL.URLByAppendingPathComponent("tempMovie", isDirectory: false).URLByAppendingPathExtension("mp4")
     }()
-    
-//        let pixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput, sourcePixelBufferAttributes: pixelBufferSettings)
-//    writer.startWriting()
-//    writer.startSessionAtSourceTime(kCMTimeZero)
 
-    
     func createAssetWriter() throws -> AVAssetWriter?
     {
         if NSFileManager.defaultManager().fileExistsAtPath(tempFileURL.path!) {
@@ -84,16 +78,28 @@ class CameraWriter : NSObject, AVCaptureVideoDataOutputSampleBufferDelegate{
         }
         let writer = try AVAssetWriter(URL: tempFileURL, fileType: AVFileTypeMPEG4)
         
-        let video:AVAssetWriterInput = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: videoWriterSettings)
-        video.expectsMediaDataInRealTime = true
-        writer.addInput(video)
-        
+        writer.addInput(videoWriter)
         return writer
     }
     
     // MARK: - AVCaptureVideoDataOutputSampleBufferDelegate
     @objc func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-        print("didOutputSampleBuffer")
+        
+        let timestamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
+        if let assetWriter = assetWriter where assetWriter.status == .Unknown {
+            assetWriter.startWriting()
+            assetWriter.startSessionAtSourceTime(timestamp)
+        }
+        
+        if CMSampleBufferDataIsReady(sampleBuffer) && videoWriter.readyForMoreMediaData && videoWriter.appendSampleBuffer(sampleBuffer) {
+            print("didOutputSampleBuffer")
+        }
+        
+        let formatDescription = CMSampleBufferGetFormatDescription(sampleBuffer)
+        let dimensions = CMVideoFormatDescriptionGetDimensions(formatDescription!)
+        
+        
+        print(CMTimeGetSeconds(timestamp))
     }
     
     @objc func captureOutput(captureOutput: AVCaptureOutput!, didDropSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
